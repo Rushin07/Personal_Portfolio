e6d068cb856d1b1fb69f512c6983a26e4df8c42c
Rushin <rushinkumar25@gmail.com>
2023-12-12 21:15:36 -0500

    structure input

--- PerPort/app/urls.py
+++ PerPort/app/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('',views.index,name='index'),
+    path('',views.home,name='home'),
     path('about', views.about, name='about'),
-    path('contact', views.contact, name='contact')
+    path('contact', views.contact, name='contact'),
+    path('services', views.services, name='services')
 ]
