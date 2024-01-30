52c43f79248fa5d01a4772ac2186dcc0ed46fa9f
Rushin07 <rushinkumar25@gmail.com>
2024-01-30 14:30:55 -0500

    debug

--- PerPort/PerPort/settings.py
+++ PerPort/PerPort/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1ea3crx8hm)q3zq^ahkq(^4)bqj7xxdoq+4z7@kiqsv74b4iyl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app']
 
