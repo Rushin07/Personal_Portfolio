cff42fe68f223c6a4d55ebfac167914c9f779d1e
Rushin <rushinkumar25@gmail.com>
2024-01-29 22:09:16 -0500

    deployment config

--- PerPort/PerPort/settings.py
+++ PerPort/PerPort/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1ea3crx8hm)q3zq^ahkq(^4)bqj7xxdoq+4z7@kiqsv74b4iyl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
