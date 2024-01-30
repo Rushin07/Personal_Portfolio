a0ab0aee83aa3c9853613907e75d33ca63deb6a0
Rushin07 <rushinkumar25@gmail.com>
2024-01-30 14:58:20 -0500

    static

--- PerPort/PerPort/settings.py
+++ PerPort/PerPort/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS=[
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATIC_ROOT = 'static/'
+STATIC_ROOT = 'Perport/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
