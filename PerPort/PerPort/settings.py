6bb0e9eb59f9f75857627a7fe1975772b8086d9a
Rushin <rushinkumar25@gmail.com>
2024-01-30 20:48:07 -0500

    database

--- PerPort/PerPort/settings.py
+++ PerPort/PerPort/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from django.contrib.messages import constants as messages
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,17 +76,15 @@ WSGI_APPLICATION = 'PerPort.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-import os
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default="sqlite:///{}".format(
-            os.path.join(BASE_DIR, 'db.sqlite3')
-        )
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
-#sync
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
