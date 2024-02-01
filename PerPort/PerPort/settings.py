7bd978679409f84333de56f08a25f6cc17df2e6c
Rushin <rushinkumar25@gmail.com>
2024-01-31 20:10:43 -0500

    final changes

--- PerPort/PerPort/settings.py
+++ PerPort/PerPort/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1ea3crx8hm)q3zq^ahkq(^4)bqj7xxdoq+4z7@kiqsv74b4iyl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1','.vercel.app','localhost','.now.sh']
 
 
 # Application definition
--- PerPort/app/models.py
+++ PerPort/app/models.py
@@ -2,18 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Contact(models.Model):
-    name=models.CharField(max_length=25)
+    name=models.CharField(max_length=50)
     email=models.EmailField()
-    phonenumber=models.CharField(max_length=10)
+    phonenumber=models.CharField(max_length=25)
     description=models.TextField()
 
     def __str__(self):
         return self.name
 
 class Blogs(models.Model):
-    title=models.CharField(max_length=60)
+    title=models.CharField(max_length=100)
     description=models.TextField()
-    authname=models.CharField(max_length=15)
+    authname=models.CharField(max_length=25)
     img=models.ImageField(upload_to='blog',blank=True,null=True)
     timestamp=models.DateTimeField(auto_now_add=True,blank=True)
     
